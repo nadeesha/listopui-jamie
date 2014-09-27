ee1b6f92fc273bfdddf53f098d0f6dc9df338779
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2014-09-27 05:52:40 +0530

    fixing ui hang

--- demo/kitchen-sink/demo.js
+++ demo/kitchen-sink/demo.js
@@ -99,20 +99,20 @@ env.split = split;
 window.env = env;
 
 
-// var consoleEl = dom.createElement("div");
-// container.parentNode.appendChild(consoleEl);
-// consoleEl.style.cssText = "position:fixed; bottom:1px; right:0;\
-// border:1px solid #baf; z-index:100";
+var consoleEl = dom.createElement("div");
+container.parentNode.appendChild(consoleEl);
+consoleEl.style.cssText = "position:fixed; bottom:1px; right:0;\
+border:1px solid #baf; z-index:100; visibility:none";
 
-// var cmdLine = new layout.singleLineEditor(consoleEl);
-// cmdLine.editor = env.editor;
+var cmdLine = new layout.singleLineEditor(consoleEl);
+cmdLine.editor = env.editor;
 // env.editor.cmdLine = cmdLine;
 
-// env.editor.showCommandLine = function(val) {
-//     this.cmdLine.focus();
-//     if (typeof val == "string")
-//         this.cmdLine.setValue(val, 1);
-// };
+env.editor.showCommandLine = function(val) {
+    this.cmdLine.focus();
+    if (typeof val == "string")
+        this.cmdLine.setValue(val, 1);
+};
 
 /**
  * This demonstrates how you can define commands and bind shortcuts to them.
